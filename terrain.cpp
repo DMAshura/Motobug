71338c957261ac9d62f4db908906fba438fab400
DMAshura <DMAshura@gmail.com>
2012-04-21 02:53:01 -0400

    * Changed some variable names for consistency. * Moved Rectangle and Triangle constructor code out of terrain.h into terrain.cpp. * Added corner_array, giving us a very easy way to draw the actual vectors in the constructors!

--- terrain.cpp
+++ terrain.cpp
@@ -1,46 +1,94 @@
 #include <SDL/SDL.h>
 #include "terrain.h"
 
-Terrain::Terrain(int X, int Y, unsigned int W, unsigned int H){
-	origin.x = X;
-	origin.y = Y;
-	w = W;
-	h = H;
+Terrain::Terrain(int x, int y, unsigned int width, unsigned int height){
+	origin.x = x;
+	origin.y = y;
+	this->width = width;
+	this->height = height;
+	corner_array[CORNER_TOPLEFT]     = Point{x,      y};
+	corner_array[CORNER_TOPRIGHT]    = Point{x+width,y};
+	corner_array[CORNER_BOTTOMRIGHT] = Point{x+width,y+height};
+	corner_array[CORNER_BOTTOMLEFT]  = Point{x,      y+height};
 }
 
-Vector* Terrain::add_vector(unsigned int x, unsigned int y, unsigned int x2, unsigned int y2) {
-Vector i(x, y, x2, y2);
-vectors.push_back(i);
-return &vectors.back();
+Vector* Terrain::add_vector(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2) {
+	Vector i(x1, y1, x2, y2);
+	vector_list.push_back(i);
+	return &vector_list.back();
 }
 
-Vector* Terrain::add_vector(unsigned int x, unsigned int y, unsigned int x2, unsigned int y2, int type) {
-Vector i(x, y, x2, y2, type);
-vectors.push_back(i);
-return &vectors.back();
+Vector* Terrain::add_vector(unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, int type) {
+	Vector i(x1, y1, x2, y2, type);
+	vector_list.push_back(i);
+	return &vector_list.back();
 }
 
-Vector* Terrain::add_vector(point v1, point v2) {
-Vector i(v1.x, v1.y, v2.x, v2.y);
-vectors.push_back(i);
-return &vectors.back();
+Vector* Terrain::add_vector(Point p1, Point p2) {
+	Vector i(p1.x, p1.y, p2.x, p2.y);
+	vector_list.push_back(i);
+	return &vector_list.back();
 }
 
-Vector* Terrain::add_vector(point v1, point v2, int type) {
-Vector i(v1.x, v1.y, v2.x, v2.y, type);
-vectors.push_back(i);
-return &vectors.back();
+Vector* Terrain::add_vector(Point p1, Point p2, int type) {
+	Vector i(p1.x, p1.y, p2.x, p2.y, type);
+	vector_list.push_back(i);
+	return &vector_list.back();
 }
 
 void Terrain::del_vector(unsigned int index) {
-vectors.erase(vectors.begin() + index);
+	vector_list.erase(vector_list.begin() + index);
 }
 
-void Terrain::move(point n_o) {
-	origin = n_o;
+void Terrain::move(point new_p) {
+	origin = new_p;
+	reset_corner_array();
 }
 
-void Terrain::resize(unsigned int nw, unsigned int nh) {
-	w = nw;
-	h = nh;
+void Terrain::reset_corner_array() {
+	corner_array[CORNER_TOPLEFT]     = Point{x,      y};
+	corner_array[CORNER_TOPRIGHT]    = Point{x+width,y};
+	corner_array[CORNER_BOTTOMRIGHT] = Point{x+width,y+height};
+	corner_array[CORNER_BOTTOMLEFT]  = Point{x,      y+height};
+}
+
+void Terrain::resize(unsigned int new_width, unsigned int new_height) {
+	width = new_width;
+	height = new_height;
+}
+
+Rectangle::Rectangle(int x, int y, unsigned int width, unsigned int height) :
+Terrain(x, y, width, height) {
+	add_vector(corner_array[CORNER_TOPLEFT],corner_array[CORNER_TOPRIGHT]);
+	add_vector(corner_array[CORNER_TOPRIGHT],corner_array[CORNER_BOTTOMRIGHT]);
+	add_vector(corner_array[CORNER_BOTTOMRIGHT],corner_array[CORNER_BOTTOMLEFT]);
+	add_vector(corner_array[CORNER_BOTTOMLEFT],corner_array[CORNER_TOPLEFT]);
+}
+
+Rectangle::Rectangle(Point p, unsigned int width, unsigned int height) :
+Terrain(p.x, p.y, width, height) {
+	add_vector(corner_array[CORNER_TOPLEFT],corner_array[CORNER_TOPRIGHT]);
+	add_vector(corner_array[CORNER_TOPRIGHT],corner_array[CORNER_BOTTOMRIGHT]);
+	add_vector(corner_array[CORNER_BOTTOMRIGHT],corner_array[CORNER_BOTTOMLEFT]);
+	add_vector(corner_array[CORNER_BOTTOMLEFT],corner_array[CORNER_TOPLEFT]);
+}
+
+Triangle::Triangle(int x, int y, unsigned int width, unsigned int height, CornerType type) :
+Terrain(x, y, width, height) {
+	// Starts with corner vertex, goes to next vertex clockwise
+	add_vector(corner_array[type],corner_array[(type + 1)%4]);
+	// Skips one vertex since it's a triangle
+	add_vector(corner_array[(type + 1)%4],corner_array[(type + 3)%4]);
+	// Returns to original vertex
+	add_vector(corner_array[(type + 3)%4],corner_array[type]);
+}
+
+Triangle::Triangle(Point p, unsigned int width, unsigned int height, CornerType type) :
+Terrain(p.x, p.y, width, height) {
+	// Starts with corner vertex, goes to next vertex clockwise
+	add_vector(corner_array[type],corner_array[(type + 1)%4]);
+	// Skips one vertex since it's a triangle
+	add_vector(corner_array[(type + 1)%4],corner_array[(type + 3)%4]);
+	// Returns to original vertex
+	add_vector(corner_array[(type + 3)%4],corner_array[type]);
 }
--- terrain.h
+++ terrain.h
@@ -5,24 +5,24 @@
 #include "mathlib.h"
 
 enum CornerType { 
-	CORNER_UPLEFT,
-	CORNER_DOWNLEFT,
-	CORNER_UPRIGHT,
-	CORNER_DOWNRIGHT
+	CORNER_TOPLEFT = 0,
+	CORNER_TOPRIGHT = 1,
+	CORNER_BOTTOMRIGHT = 2,
+	CORNER_BOTTOMLEFT = 3
 };
 
 class Terrain {
 	public:
-		Terrain(int X, int Y, unsigned int W, unsigned int H);
-		Terrain(point o, unsigned int W, unsigned int H);
+		Terrain(int x, int y, unsigned int width, unsigned int height);
+		//Terrain(Point p, unsigned int width, unsigned int height);
 		virtual ~Terrain();
 		
 		//Misc Functions
 		virtual bool blit();
 		
 		//Geometry management functions
-		Vector* add_vector(point v1, point v2);
-		Vector* add_vector(point v1, point v2, int type);
+		Vector* add_vector(Point p1, Point p2);
+		Vector* add_vector(Point p1, Point p2, int type);
 		Vector* add_vector(unsigned, unsigned, unsigned, unsigned);
 		Vector* add_vector(unsigned, unsigned, unsigned, unsigned, int);
 		
@@ -33,49 +33,30 @@ class Terrain {
 		//virtual void del_vertex(unsigned int index);
 		//virtual void mov_vertex(unsigned int index, unsigned int nx, unsigned int ny);
 
-		virtual void resize(unsigned int nw, unsigned int nh);
-		virtual void move(point n_o);
+		virtual void resize(unsigned int new_width, unsigned int new_height);
+		virtual void move(point new_p);
+		void reset_corner_array();
 	
 		//std::vector<point> vertices;
-		std::vector<Vector> vectors;
+		static std::vector<Vector> vector_list;
 		
 	protected:
-		point origin;
-		unsigned int w, h;
+		Point origin;
+		unsigned int width, height;
+		Point corner_array[4];
 };
 
 class Rectangle: protected Terrain {
 	public:
-		Rectangle(int x, int y, unsigned int w, unsigned int h) :
-		Terrain(x,y,w,h) {
-		add_vector(x,y,x+w,y);
-		add_vector(x+w,y,x+w,y+h);
-		add_vector(x+w,y+h,x,y+h);
-		add_vector(x,y+h,x,y);
-		}
-		
-		Rectangle(point o, unsigned int w, unsigned int h) :
-		Terrain(o,w,h) {
-		add_vector(x,y,x+w,y);
-		add_vector(x+w,y,x+w,y+h);
-		add_vector(x+w,y+h,x,y+h);
-		add_vector(x,y+h,x,y);
-		};
-		
+		Rectangle(int x, int y, unsigned int width, unsigned int height);
+		Rectangle(Point p, unsigned int width, unsigned int height);
 		virtual ~Rectangle();
 };
 
 class Triangle: protected Terrain {
 	public:		
-		Triangle(int X, int Y, unsigned int W, unsigned int H, CornerType tritype) :
-		Terrain(X,Y,W,H) {
-		//code
-		}
-		Triangle(point o, unsigned int w, unsigned int h, CornerType tritype) :
-		Terrain(o,w,h) {
-		//code
-		}
-		
+		Triangle(int x, int y, unsigned int width, unsigned int height, CornerType type);
+		Triangle(Point p, unsigned int width, unsigned int height, CornerType type)
 		virtual ~Triangle();
 	private:
 		CornerType type;
